cb078c7c33d5f77db3f8b0d9b8b4b860b4cbd662
dco0 <decordofgammazeroplusone@gmail.com>
2026-07-14 10:42:16 -0400

    visual changes

--- src/hyper.ts
+++ src/hyper.ts
@@ -59,6 +59,8 @@ export class PoincareDiskRenderer {
     drawDisk(): void {
         this.ctx.beginPath();
         this.ctx.ellipse(this.cx, this.cy, this.s, this.s, 0, 0, 2*Math.PI);
+        this.ctx.lineWidth = 6;
+        this.ctx.strokeStyle = "black";
         this.ctx.stroke();
     }
     drawLine(l: vec3): void {
